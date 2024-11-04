819c023dd2ec3263888849b65146e63ee4d92d9c
AswinM-programmer07 <144223299+AswinM-programmer07@users.noreply.github.com>
2024-11-03 17:16:22 -0800

    Add files via upload

---
+++ tripscript.js
@@ -0,0 +1,29 @@
+const totalFoodTrip = localStorage.getItem("totalFoodTrip");
+
+// Use the total value as needed (e.g., logging to the console)
+if (totalFoodTrip !== null) {
+    console.log("Total Food Trip Cost:", totalFoodTrip);
+} else {
+    console.log("No total food trip cost found.");
+}
+
+const totaltravelTrip = localStorage.getItem("totalFoodTrip");
+
+// Use the total value as needed (e.g., logging to the console)
+if (totalFoodTrip !== null) {
+    console.log("Total Food Trip Cost:", totalFoodTrip);
+} else {
+    console.log("No total food trip cost found.");
+}
+const totalacoomodationTrip = localStorage.getItem("totalFoodTrip");
+
+// Use the total value as needed (e.g., logging to the console)
+if (totalFoodTrip !== null) {
+    console.log("Total Food Trip Cost:", totalFoodTrip);
+} else {
+    console.log("No total food trip cost found.");
+}
+
+const other = 0;
+
+const entertainement = 0
